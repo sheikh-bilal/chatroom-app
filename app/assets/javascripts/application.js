565b39047339b2b649615d8b036007b71467e502
bilalahm3d <b.ahmad@devsinc.com>
2021-04-02 18:57:05 +0500

    SemanticUI: use semantic UI & favicon

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,15 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require semantic-ui
 //= require_tree .
+
+$(document).on('turbolinks:load', function(){
+  $('.ui.dropdown')
+    .dropdown()
+  ;
+})
