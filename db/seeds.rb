58a37f3425737c25ee8f9548a6134e9592ded53f
bilalahm3d <b.ahmad@devsinc.com>
2021-04-03 00:25:29 +0500

    Partial & Db: Use partial for message and display users messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# User creation
+User.create(username: 'Bilal', password: 'password')
+User.create(username: 'Abdullah', password: 'password')
+User.create(username: 'Fariha', password: 'password')
+
+Message.create(body: "Hello Everyone how are you?", user_id: 1)
+Message.create(body: "Hey My friends?", user_id: 2)
+Message.create(body: "I am giving you all Magnum", user_id: 3)
