388cc46b91a7d0b75ef0670887eebefabf1831a9
bilalahm3d <b.ahmad@devsinc.com>
2021-04-04 00:50:07 +0500

    Database: setup postgresql instead of sqlite3

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_04_02_182245) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "messages", force: :cascade do |t|
     t.text "body", null: false
     t.integer "user_id", null: false
